01626199d90e9168aa37782dd175577bfa40c185
kiedrowskipio <kiedrowskipio@gmail.com>
2023-12-01 13:52:32 +0100

    rpm counter setup

--- CruiseControl/CruiseControl.ino
+++ CruiseControl/CruiseControl.ino
@@ -1,10 +1,26 @@
-void setup() {
-  // put your setup code here, to run once:
+#define CALTULATION_INTERVAL 2500 // millisecond delay between calculating rpm 
+#define PULSE_PER_REV 2 // number of magnets on the drive-shaft
+unsigned long stopWatch = 0;
+volatile int count = 0;
+int rpm = 0;
+const int scale = 60000/CALCULATION_INTERVAL/PUSE_PER_REV;
 
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(2), incrementCount, FALLING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(millis() - stopWatch > CALCULATION_INTERVAL){
+    rpm = count*scale;
+    Serial.println(rpm);
+    count = 0;
+    stopWatch = millis();
+  }
 
+}
+
+void incrementCount(){
+  count++;
 }
 
