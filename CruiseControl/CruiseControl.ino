95d115ecb366ccc54982b67ecf94e92b4956054c
kiedrowskipio <kiedrowskipio@gmail.com>
2023-12-07 12:17:17 +0100

    merge of two files and begginig of car configuration

--- CruiseControl/CruiseControl.ino
+++ CruiseControl/CruiseControl.ino
@@ -4,6 +4,19 @@ unsigned long stopWatch = 0;
 volatile int count = 0;
 int rpm = 0;
 const int scale = 60000/CALCULATION_INTERVAL/PUSE_PER_REV;
+// vehicle configuration
+const int highestGearRatio = 1; //My car has direct drive on its highest gear - needs to be changed accorging to vehicle specs
+const double differentialRatio = 4,3; //Has to be changed according to vehicle specs 
+const int wheelCircumference=
+///
+int vehicleSpeed = 0;
+
+/// stepper motor controll ///
+const int stepsPerRev = 200; //Has to be changed accordingly to the motor
+Stepper myStepper(stepsPerRev, 8, 9, 10, 11); //Initialization of the stepper motor
+int stepCount = 0; 
+///
+
 
 void setup() {
   Serial.begin(9600);
@@ -13,10 +26,15 @@ void setup() {
 void loop() {
   if(millis() - stopWatch > CALCULATION_INTERVAL){
     rpm = count*scale;
-    Serial.println(rpm);
+    Serial.print("Driveshaft speed:");
+    Serial.print(rpm);
+    Serial.println("rpm");
+    
     count = 0;
     stopWatch = millis();
   }
+
+  myStepper.step(stepsPerRev / 100); //step 1% of revolution
 
 }
 
