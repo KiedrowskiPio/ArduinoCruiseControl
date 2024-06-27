863e121a7c918b103545c2d31b7e767110c3f3ee
kiedrowskipio <kiedrowskipio@gmail.com>
2024-06-27 17:05:47 +0200

    added rotary encoder support

--- CruiseControl/CruiseControl.ino
+++ CruiseControl/CruiseControl.ino
@@ -1,9 +1,21 @@
-#include <Stepper.h> //Stepper motor controling library
-#define CALTULATION_INTERVAL 2500 // millisecond delay between calculating rpm 
-#define PULSE_PER_REV 2 // number of magnets on the drive-shaft
-#define BRAKE_PEDAL_INPUT 3 //input from brake pedal
+//Include mandatory libraries
+#include <Stepper.h>
+#include <Encoder.h>
+#include <Arduino.h>
+#include <HallA1302.h>
+
+//Pin Definition
+#define BRAKE_PEDAL_INPUT 4 //input from brake pedal
+#define HALL_PIN_VOUT A0
+#define ROTARTENCI_PIN_CLK 2
+#define ROTARTENCI_PIN_D 3
+#define ROTARTENCI_PIN_S1 5
+#define STEPPER_PIN_STEP 7
+#define STEPPER_PIN_DIR 6
 
 //Driveshaft Speed calculation
+const int PULSE_PER_REV = 2; //number of magnets on driveshaft
+const int CALCULATION_INTERVAL = 2500; // millisecond delay between calculating rpm 
 unsigned long stopWatch = 0;
 volatile int count = 0;
 int rpm = 0;
@@ -18,8 +30,7 @@ int vehicleSpeed = 0; //calculated vehicle speed in km/h
 int savedSpeed = 0; //speed saved from previous cycle
 
 //stepper motor control
-const int stepsPerRev = 200; //Has to be changed accordingly to the motor
-Stepper myStepper(stepsPerRev, 8, 9, 10, 11); //Initialization of the stepper motor
+StepperMotor myStepper(STEPPER_PIN_STEP, STEPPER_PIN_DIR); //Initialization of the stepper motor
 int stepCount = 0; 
 int motorSpeed = 720;
 
@@ -27,7 +38,14 @@ int motorSpeed = 720;
 int setSpeed = 0; //speed desired to maintain
 bool systemActive = false; //system working flag
 
+//Object initialization
+Encoder rotartEnc1(ROTARTENCI_PIN_D, ROTARTENCI_PIN_CLK);
+HallA1302 hallA(HALLA_PIN_COUT);
+long rotaryEncIOldPosition  = 0;
+
 void setup() {
+  rotaryEncIButton.init();
+  myStepper.enable();
   combinedRatio = highestGearRatio/differentialRatio*wheelCircumference/100;
   myStepper.setSpeed(motorSpeed);
   Serial.begin(9600);
@@ -35,6 +53,14 @@ void setup() {
 }
 
 void loop() {
+
+  long rotaryEncINewPosition = rotaryEncI.read();
+  if (rotaryEncINewPosition != rotaryEncIOldPosition) {
+    rotaryEncIOldPosition = rotaryEncINewPosition;
+    setSpeed = rotaryEncINewPosition;
+    Serial.print(F("Wybrana prędkość: "));
+    Serial.print(rotaryEncINewPosition);
+
   if(millis() - stopWatch > CALCULATION_INTERVAL){
     rpm = count*scale;
     Serial.print("Driveshaft speed: ");
@@ -46,7 +72,7 @@ void loop() {
     Serial.println("");
     
     count = 0;
-    stopWatch = millis();t
+    stopWatch = millis();
   }
   if(systemActive = true){
       if (setSpeed < vehicleSpeed-4 || setSpeed > vehicleSpeed+4)
@@ -58,8 +84,8 @@ void loop() {
           Serial.println("vehicle is accelerating")
         }
         else{
-          myStepper.step(stepsPerRev / 50); //step 2% of revolution
-          Serial.println("vehicle speed is too slow. Speeding up...")
+          myStepper.step(1, 5);
+          Serial.println("vehicle velocity is too low. Speeding up...")
         }
       }
       else if(setSpeed < vehicleSpeed){
@@ -67,13 +93,12 @@ void loop() {
           Serial.println("vehicle is slowing down")
         }
         else{
-          myStepper.step(-stepsPerRev / 25); //step 4% of revolution back
+          myStepper.step(0, 5);
           Serial.println("vehicle speed is too great. Slowing down...")
         }
 
       }
   }
-
 
 
 delay(2000);
